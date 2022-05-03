31dfc3e58e363f37297afa6f87143e18afb7bd42
Nathan Dereux <104099131+NathanduTP@users.noreply.github.com>
2022-05-03 19:16:29 +0200

    modifs visuelles sur PlayerHomeVue

--- src/views/PlayerHomeView.vue
+++ src/views/PlayerHomeView.vue
@@ -1,50 +1,38 @@
 <template>
-  <div class="items-center justify-center w-full h-screen">
-    <div
-      class="container flex flex-col items-center justify-center h-full max-w-6xl pl-0 mx-auto -mt-24 sm:pl-8 xl:pl-0 md:flex-row md:justify-between"
-    >
-      <div
-        class="flex flex-col items-center w-5/6 md:items-start sm:w-2/3 lg:w-3/8 lg:mt-10"
-      >
-        <div>
-          <h1
-            class="mb-4 text-5xl font-black leading-none text-center text-white lg:text-6xl xl:text-7xl md:text-left capitalize"
-          >
-            battle royale de la culture générale
-          </h1>
+  <div class="root">
+    <div class="logo">
+      <img
+        src="../assets/images/logo-battle-royale-culture-g.png"
+        alt="Battle Royale de la Culture Générale"
+        width="110%"
+      />
+    </div>
 
-          <p
-            class="my-3 text-base text-center text-gray-600 xl:text-xl md:text-left"
-          >
-            Done my random members of Club Info
-          </p>
-        </div>
+    <div class="pseudo">Pseudo :</div>
 
-        <div class="flex gap-4 mt-5">
-          <div class="fancy-shadow rounded-3xl">
-            <input
-              @change="onPseudoChange"
-              placeholder="Pseudo"
-              class="text-center outline-none w-full h-full px-2 py-3 text-base font-bold bg-white border-2 border-[#5E17EB] rounded-3xl xl:text-xl fold-bold"
-            />
-          </div>
+    <div class="form">
+      <input
+        @change="onPseudoChange"
+        type="text"
+        name="pseudo-input"
+        class="pseudo-input"
+        placeholder="Pseudo"
+      />
+    </div>
 
-          <button
-            @click="onGoClick"
-            class="bg-[#E40495] border-[#5E17EB] border-2 rounded-3xl fancy-shadow"
-          >
-            <span
-              class="text-white w-full h-full px-8 py-3 text-base font-bold xl:text-xl fold-bold uppercase"
-              >go</span
-            >
-          </button>
-        </div>
-      </div>
+    <div class="butt">
+      <button @click="onGoClick" class="button">
+        <span>GO !</span>
+      </button>
+    </div>
 
-      <div
-        class="flex flex-col items-end justify-center w-5/6 h-auto pl-0 pr-0 mt-10 sm:pl-20 sm:pr-8 xl:pr-0 md:mt-0 md:h-full sm:w-2/3"
-      >
-        <logo-club-info />
+    <div class="logo1">
+      <div class="logos">
+        <img
+          src="../assets/images/logo-club-culture-inge.png"
+          alt="Logo du club culture ingé"
+        />
+        <img src="../assets/images/hero-logo.svg" alt="Logo du club info" />
       </div>
     </div>
   </div>
